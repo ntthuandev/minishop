df55f0c1a1ac3dcd431e0d5ba42e7af5aea95313
ntthuandev <fe.thanhthuan@gmail.com>
2023-12-19 23:57:49 +0700

    refixed2

--- frontend/src/config/Url.js
+++ frontend/src/config/Url.js
@@ -1,3 +1,3 @@
-// export const API_URL = "https://minishop-server.onrender.com/api"
+export const API_URL = "https://minishop-server.onrender.com/api"
 
-export const API_URL = "http://localhost:8080/api"
+// export const API_URL = "http://localhost:8080/api"
