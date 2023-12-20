21b41cc4180756ea099718d6b0ae0db07c54a651
ntthuandev <fe.thanhthuan@gmail.com>
2023-12-20 18:53:36 +0700

    fixed cookie

--- server/index.js
+++ server/index.js
@@ -20,13 +20,7 @@ const allowedOrigins = [
     'https://minishop-eight.vercel.app'
 ];
 const corsOptions = {
-    origin: (origin, callback) => {
-        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-            callback(null, true)
-        } else {
-            callback(new Error('Not allowed by CORS'))
-        }
-    },
+    origin:allowedOrigins,
     credentials: true,
     optionsSuccessStatus: 200
 }
