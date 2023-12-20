a21abca5d72d2b8ae891dbc99481a959b1400394
ntthuandev <fe.thanhthuan@gmail.com>
2023-12-20 17:36:35 +0700

    refixed

--- frontend/src/config/Url.js
+++ frontend/src/config/Url.js
@@ -1,3 +1,4 @@
-export const API_URL = "https://minishop-server.onrender.com/api"
+// export const API_URL = "https://minishop-server.onrender.com/api"
+export const API_URL = "https://minishop-server-k10n89iks-thuans-projects-3f03f8f9.vercel.app/api"
 
 // export const API_URL = "http://localkhost:8080/api"
