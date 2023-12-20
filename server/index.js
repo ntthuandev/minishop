064a7ce80c02e1348d5c704f467dd2ae02c6f2dd
ntthuandev <fe.thanhthuan@gmail.com>
2023-12-20 18:49:56 +0700

    refixed cookie

--- server/index.js
+++ server/index.js
@@ -34,8 +34,11 @@ const corsOptions = {
 const POST = process.env.POST || 8080
 // app.set("trust proxy", 1)
 app.use(cors(corsOptions))
-app.use(cookieParser())
-app.use(express.urlencoded())
+app.use(
+    express.urlencoded({
+      extended: true,
+    }),
+  );
 app.use(express.json());
 // add middleware
 app.use("/api/auth", authRoute)
