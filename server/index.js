efb99bf6aeabc07d6b2cd3190572cf746f6bb698
ntthuandev <fe.thanhthuan@gmail.com>
2023-12-20 18:04:04 +0700

    fixed

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const app = express();
 //config app
 dotenv.config();
 const corsOptions ={
-     origin:'https://minishop-eight.vercel.app', 
+     origin:['https://minishop-eight.vercel.app', 'http://localhost:3000' ], 
     // origin:['https://minishop-frontend.onrender.com', 'http://localhost:3000', "https://minishop-eight.vercel.app"], 
     credentials:true           //access-control-allow-credentials:true
 }
